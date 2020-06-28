4a281d9d51521f6c7678e0958dc39459b373c9a1
slamatik <vjaceslavsmatess@gmail.com>
2020-06-28 16:46:42 +0100

    solution modified code from #7, not optimised

---
+++ 10. Summation of primes.py
@@ -0,0 +1,28 @@
+from math import sqrt
+
+
+def prime_check(n):
+    if n == 2:
+        return True
+    if n % 2 == 0 or n <= 1:
+        return False
+
+    limit = int(sqrt(n) + 1)
+
+    for div in range(3, limit, 2):
+        if n % div == 0:
+            return False
+    return True
+
+
+total = 0
+
+cnt, prime = 1, 1
+while cnt < 200000:
+    prime += 2
+    if prime_check(prime):
+        cnt += 1
+        total += prime
+    if prime > 2_000_000:
+        break
+print(total + 2)
