d817f2dfeecb35aa949b8bb8d5f8ada00c9326cd
slamatik <64711169+slamatik@users.noreply.github.com>
2020-06-28 14:06:20 +0100

    Update 6. Sum square difference.py

--- 6. Sum square difference.py
+++ 6. Sum square difference.py
@@ -1,7 +1,1 @@
-solution = False
-n = 20
-while not solution:
-    if n % 11 == 0 and n % 12 == 0 and n % 13 == 0 and n % 14 == 0 and n % 15 == 0 and n % 16 == 0 and n % 17 == 0 and n % 18 == 0 and n % 19 == 0 and n % 20 == 0:
-        print(n)
-        solution = True
-    n += 20
+
