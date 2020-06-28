e6d552efcbbc49e7ec51dc965e462382681ba2b5
slamatik <vjaceslavsmatess@gmail.com>
2020-06-28 14:10:11 +0100

    solution

--- 6. Sum square difference.py
+++ 6. Sum square difference.py
@@ -1,1 +1,15 @@
+def find_sum_squares(n):
+    total = 0
+    for i in range(n + 1):
+        total += i ** 2
+    return total
 
+
+def find_square_of_sum(n):
+    total = 0
+    for i in range(n + 1):
+        total += i
+    return total ** 2
+
+
+print(find_square_of_sum(100) - find_sum_squares(100))
